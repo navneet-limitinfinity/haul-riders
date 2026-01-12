419bb737778b4ff00862ae8202fc3f163d1ed462
navneet-sinha <navneet@limitinfinity.com>
2026-01-12 20:47:17 +0530

    chore(design): relax JSX & any rules for web preview (allow modern JSX, warn on any)

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -23,7 +23,9 @@ module.exports = {
   rules: {
     // project preferences
     'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'no-console': ['warn', { allow: ['warn', 'error'] }]
   }
